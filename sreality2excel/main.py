99ebc69ab90d31ee1cbcae809f56205d433b0ab1
Jan Mayer <jan-mayer@outlook.com>
2021-07-18 13:10:00 +0200

    better main loop

--- sreality2excel/main.py
+++ sreality2excel/main.py
@@ -2,6 +2,9 @@ import openpyxl as opx
 from sreality_data import Advertisment
 from excel_utils import first_available_row
 from enum import Enum
+from sys import argv
+from itertools import count
+from openpyxl.worksheet.worksheet import Worksheet
 
 DATA_SHEET_PATH = '../workbooks/data_FINAL_IQ_2021_Adam.xlsx'
 ATTRIBUTES = (
@@ -26,12 +29,9 @@ ATTRIBUTES = (
 )
 COLUMN_MAP = {attr: i + 3 for i, attr in enumerate(ATTRIBUTES)}
 
-def main():
+def process_ad(ws: Worksheet, row_idx: int) -> None:
     url = input('Insert the advertisement URL:\n')
-    #url = 'https://www.sreality.cz/detail/prodej/byt/1+kk/praha-zizkov-biskupcova/3890874972#img=0&fullscreen=false'
     ad = Advertisment(url)
-    wb = opx.load_workbook(DATA_SHEET_PATH)
-    ws = wb['data']
     row = {}
     for attr, col in COLUMN_MAP.items():
         try:
@@ -50,8 +50,22 @@ def main():
     row_idx = first_available_row(ws)
     for col, val in row.items():
         ws.cell(row_idx, col, val)
-    wb.save(DATA_SHEET_PATH)
-    print('ad successfully saved')
+
+def main():
+    wb = opx.load_workbook(DATA_SHEET_PATH)
+    ws = wb['data']
+    row_idx = first_available_row(ws)
+    idx_iter = [row_idx]
+    if len(argv) > 1 and argv[1] == '-m':
+        idx_iter = count(row_idx)
+    try:
+        for idx in idx_iter:
+            process_ad(ws, idx)
+    except KeyboardInterrupt:
+        print('terminated by the user')
+    finally:
+        wb.save(DATA_SHEET_PATH)
+        print('new entries successfully saved')
 
 if __name__ == '__main__':
     main()
